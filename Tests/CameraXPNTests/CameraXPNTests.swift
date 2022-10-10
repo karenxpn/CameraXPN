8c1d738f24026cb06973daa9fdb199fbfb59cb7c
KALIMI <karenmirakyan@gmail.com>
2022-10-11 01:09:25 +0400

    tests example removed

--- Tests/CameraXPNTests/CameraXPNTests.swift
+++ Tests/CameraXPNTests/CameraXPNTests.swift
@@ -1,11 +1,7 @@
 import XCTest
+import SwiftUI
 @testable import CameraXPN
 
 final class CameraXPNTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CameraXPN().text, "Hello, World!")
-    }
+
 }
